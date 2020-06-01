919b5a1b6cb3ac9169b359a1553ba487246a3739
VolDemorT-acer <58597818+VolDemorT-acer@users.noreply.github.com>
2020-06-01 12:37:22 +0530

    update

--- js/index.js
+++ js/index.js
@@ -16,6 +16,35 @@ var swiper = new Swiper('.swiper-container', {
      el: '.swiper-pagination',
    },
  });
+
+
+
+
+
+ var burger=document.querySelector(".burger-action") ;
+var lines=document.querySelectorAll(".burger-action div") ;
+
+var nav=document.querySelector(".navigation-menu");
+burger.addEventListener("click",()=>{
+burgerchange() ;
+nav.classList.toggle("nav-active") ;
+})
+
+
+function burgerchange(){
+  lines.forEach((l,index)=>{
+
+    l.classList.toggle(`line${index+1}-active`) ;
+    })
+}
+
+
+
+
+
+
+
+
  var switcher= document.querySelectorAll('.project .picker ul li') ;
  var swipecontainer=document.querySelectorAll('.project .project-switcher .swiper-container') ;
 
@@ -35,22 +64,47 @@ var swiper = new Swiper('.swiper-container', {
        
      })
  })
+
+
+
+
+
+
+
+
  gsap.registerPlugin(ScrollToPlugin);
 var contact=document.querySelector('header .navbar ul li button').addEventListener("click"
 ,(e)=>{
   e.preventDefault() ;
-  gsap.to(window,{duration:1.5,scrollTo:{y:"#section6",offsetY:100,ease:Power3.inout}})
+  burgerchange() ;
+  nav.classList.toggle("nav-active") ;
+  gsap.to(window,{duration:1.5,scrollTo:{y:"#section6",offsetY:90,ease:Power3.inout}})
   
 })
 var skill=document.querySelector('header .navbar ul .skillsection a').addEventListener("click"
 ,(e)=>{
   e.preventDefault() ;
-  gsap.to(window,{duration:1,scrollTo:{y:"#section3",offsetY:100}})
+  burgerchange() ;
+  nav.classList.toggle("nav-active") ;
+  gsap.to(window,{duration:1,scrollTo:{y:"#section3",offsetY:80}})
   
 })
 var project=document.querySelector('header .navbar ul .projectsection a').addEventListener("click"
 ,(e)=>{
   e.preventDefault() ;
-  gsap.to(window,{duration:.5,scrollTo:{y:"#section2",offsetY:100}})
+  burgerchange() ;
+  nav.classList.toggle("nav-active") ;
+  gsap.to(window,{duration:.5,scrollTo:{y:"#section2",offsetY:80}})
   
 })
+
+var home=document.querySelector('header .navbar ul .home a').addEventListener("click"
+,(e)=>{
+  e.preventDefault() ;
+  burgerchange() ;
+  nav.classList.toggle("nav-active") ;
+  gsap.to(window,{duration:.5,scrollTo:{y:"header",offsetY:90}})
+  
+})
+
+
